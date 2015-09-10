cc47f0150bbd44cacbc0986efa9a78296201f49b
ab <adamdd55@gmail.com>
2015-09-10 17:02:09 +0200

    step1 - introduction to safer refactoring

--- src/main/scala/com/example/Cart.scala
+++ src/main/scala/com/example/Cart.scala
@@ -2,15 +2,35 @@ package com.example
 
 object Cart {
 
-  type stockItem = String
-  val stockPrices = Map[stockItem, Int]("Apple" -> 60, "Orange" -> 25)
+  type StockItem = String
+  val stockPrices = Map[StockItem, Int]("Apple" -> 60, "Orange" -> 25)
 
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
   }
 
-  def getPrice(in: List[stockItem]): Int = in.foldLeft(0){(basketValue, item) =>
+  def getPrice(in: List[StockItem]): Int = in.foldLeft(0){(basketValue, item) =>
     basketValue + stockPrices(item)
   }
+
+  // Later we realized we want more safety on item types.
+  // It is a first phase of refactoring, so let's introduce a bit better type
+  /**
+   * A better containter for product
+   * @param itemName Name of the product
+   */
+  case class SaferStockItem(itemName: String) {
+    // that will be validated with our stock base
+    assert(stockPrices.contains(itemName))
+
+    // stock elements may come from different systems, they are not hard-coded here,
+    // sometimes we may even need to match similar items as a one
+    // In a typical shop, we would rather have some codes/hashes for our products,
+    // so approach with case class allow us to migrate into such scenario too
+  }
+
+  // We just introduced new type, but we do not need to modify
+  // essential core of our computation system yet (low risk change)
+  implicit def saferListStockItemToListStockItem(in: List[SaferStockItem]): List[StockItem] = in.map(_.itemName)
 
 }
--- src/test/scala/CartSpec.scala
+++ src/test/scala/CartSpec.scala
@@ -1,5 +1,5 @@
 import com.example.Cart
-import com.example.Cart.stockItem
+import com.example.Cart.{SaferStockItem, StockItem}
 import org.scalatest._
 
 class CartSpec extends FlatSpec with Matchers {
@@ -8,10 +8,18 @@ class CartSpec extends FlatSpec with Matchers {
   }
 
   "Cart" should "compute prices" in {
-    val basket1 = List[stockItem]("Orange", "Apple")
+    val basket1 = List[StockItem]("Orange", "Apple")
     Cart.getPrice(basket1) shouldBe (Cart.stockPrices("Apple") + Cart.stockPrices("Orange"))
 
-    val basket2 = List[stockItem]("Orange", "Apple", "Orange", "Orange")
+    val basket2 = List[StockItem]("Orange", "Apple", "Orange", "Orange")
     Cart.getPrice(basket2) shouldBe (Cart.stockPrices("Apple") + 3 * Cart.stockPrices("Orange"))
+  }
+
+  "Cart" should "computer prices with extended types" in {
+    val basket3 = List[SaferStockItem](
+      SaferStockItem("Orange"),
+       SaferStockItem("Apple")
+      )
+    Cart.getPrice(basket3) shouldBe (Cart.stockPrices("Apple") + Cart.stockPrices("Orange"))
   }
 }
